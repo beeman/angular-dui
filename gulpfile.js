a5a75d2e6e1f3aea39a51ad7b9b377a6e40b2f4f
Bram Borggreve <borggreve@gmail.com>
2015-05-20 02:51:48 +0200

    Add build proces

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var angularTemplates = require('gulp-angular-templates');
+var del = require('del');
+var concat = require('gulp-concat');
 
 var watchDirs = [
   '*',
-  'js/*.js',
-  'templates/*.html',
+  'src/js/*.js',
+  'src/templates/*.html',
   'examples/**/*.*'
 ];
 
@@ -21,3 +24,33 @@ gulp.task('serve', function () {
 
   gulp.watch(watchDirs, {cwd: './'}, reload);
 });
+
+gulp.task('angular', function () {
+  return gulp.src('src/templates/**/*.html')
+    .pipe(angularTemplates({module: 'angularDui', basePath: '/templates/'}))
+    .pipe(gulp.dest('./build/'));
+});
+
+// Scripts
+gulp.task('scripts', function () {
+  return gulp.src(['src/js/**/*.js', 'build/*.html.js'])
+//    .pipe(jshint('.jshintrc'))
+//    .pipe(jshint.reporter('default'))
+    .pipe(concat('angular-dui.js'))
+    //.pipe(gulp.dest('dist'))
+    //.pipe(rename({suffix: '.min'}))
+    //.pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+// Clean
+gulp.task('clean', function (cb) {
+  del(['dist/', 'build/'], cb);
+});
+
+// Default task
+gulp.task('default', ['clean', 'angular'], function () {
+  gulp.start('scripts');
+});
+
+
